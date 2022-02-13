3cdac3424661d93959c766d0122bd2ffbba95056
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-02-13 16:33:08 +0000

    Seamless loading of stories.

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ import (
 const apiurl = "https://hacker-news.firebaseio.com/v0"
 
 type story struct {
-	id          int
+	id          int // -1 when not loaded
 	by          string
 	time        int
 	timestr     string
@@ -81,6 +81,41 @@ func fetchTopStories() tea.Msg {
 
 func (m model) Init() tea.Cmd {
 	return fetchTopStories
+}
+
+func timestampToString(timestamp int64) string {
+	diff := int64(time.Now().UTC().Sub(time.Unix(timestamp, 0)).Seconds())
+	if diff < 60 {
+		return fmt.Sprintf("%d seconds ago", diff)
+	}
+	diff = diff / 60
+	if diff < 60 {
+		return fmt.Sprintf("%d minutes ago", diff)
+	}
+	diff = diff / 60
+	if diff < 60 {
+		return fmt.Sprintf("%d hours ago", diff)
+	}
+	diff = diff / 24
+	if diff == 1 {
+		return "a day ago"
+	} else if diff < 7 {
+		return fmt.Sprintf("%d days ago", diff)
+	} else if diff < 30 {
+		diff = diff / 7
+		return fmt.Sprintf("%d weeks ago", diff)
+	}
+	diff = diff / 30
+	if diff == 1 {
+		return "a month ago"
+	} else if diff < 12 {
+		return fmt.Sprintf("%d months ago", diff)
+	}
+	diff = diff / 365
+	if diff == 1 {
+		return "a year ago"
+	}
+	return fmt.Sprintf("%d years ago", diff)
 }
 
 func fetchStory(item string) tea.Cmd {
@@ -168,38 +203,61 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		return m, tea.Quit
 	case topStoriesMsg:
 		m.topstories = msg.stories
-		return m, tea.Batch(
-			fetchStory(strconv.Itoa(m.topstories[0])),
-			fetchStory(strconv.Itoa(m.topstories[1])),
-			fetchStory(strconv.Itoa(m.topstories[2])),
-			fetchStory(strconv.Itoa(m.topstories[3])),
-			fetchStory(strconv.Itoa(m.topstories[4])),
-		)
+		var batch [5]tea.Cmd
+		for i := 0; i < len(m.topstories) && i < 5; i++ {
+			stId := m.topstories[i]
+			m.stories[stId] = story{id: -1}
+			batch[i] = fetchStory(strconv.Itoa(stId))
+		}
+		return m, tea.Batch(batch[:]...)
 	case story:
 		m.stories[msg.id] = msg
-		if len(m.stories) >= 5 {
-			m.state = 1
-		}
+		// we have a story => we're ready
+		m.state = 1
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "q":
-			return m, tea.Quit
-		case "down", "j":
-			if m.cursor < len(m.stories)-1 {
-				m.cursor++
-			}
-		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
-			}
-		case "enter", "right", "l":
-			if m.state == 1 {
+		switch m.state {
+		case 1:
+			switch msg.String() {
+			case "ctrl+c", "q":
+				return m, tea.Quit
+			case "down", "j":
+				if m.cursor < len(m.topstories)-1 {
+					m.cursor++
+					// load missing stories
+					var batch []tea.Cmd
+					for i := m.cursor; i < len(m.topstories) && i < m.cursor+5; i++ {
+						stId := m.topstories[i]
+						_, exists := m.stories[stId]
+						if !exists {
+							batch = append(batch, fetchStory(strconv.Itoa(stId)))
+						}
+					}
+					return m, tea.Batch(batch...)
+				}
+			case "up", "k":
+				if m.cursor > 0 {
+					m.cursor--
+				}
+			case "enter", "right", "l":
 				m.selected = m.cursor
-				m.state = 2
+				stId := m.topstories[m.selected]
+				st, exists := m.stories[stId]
+				if exists && st.id > 0 {
+					// loaded => we can go in
+					m.state = 2
+				}
 			}
-		case "left", "h":
-			if m.state == 2 {
+		case 2:
+			switch msg.String() {
+			case "ctrl+c", "q":
+				return m, tea.Quit
+			case "left", "h":
 				m.state = 1
+			}
+		default:
+			switch msg.String() {
+			case "ctrl+c", "q":
+				return m, tea.Quit
 			}
 		}
 	}
@@ -207,66 +265,33 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, nil
 }
 
-func timestampToString(timestamp int64) string {
-	diff := int64(time.Now().UTC().Sub(time.Unix(timestamp, 0)).Seconds())
-	if diff < 60 {
-		return fmt.Sprintf("%d seconds ago", diff)
-	}
-	diff = int64(diff / 60)
-	if diff < 60 {
-		return fmt.Sprintf("%d minutes ago", diff)
-	}
-	diff = int64(diff / 60)
-	if diff < 60 {
-		return fmt.Sprintf("%d hours ago", diff)
-	}
-	diff = int64(diff / 24)
-	if diff == 1 {
-		return "a day ago"
-	} else if diff < 7 {
-		return fmt.Sprintf("%d days ago", diff)
-	} else if diff < 30 {
-		diff = int64(diff / 7)
-		return fmt.Sprintf("%d weeks ago", diff)
-	}
-	diff = int64(diff / 30)
-	if diff == 1 {
-		return "a month ago"
-	} else if diff < 12 {
-		return fmt.Sprintf("%d months ago", diff)
-	}
-	diff = int64(diff / 365)
-	if diff == 1 {
-		return "a year ago"
-	}
-	return fmt.Sprintf("%d years ago", diff)
-}
-
 func (m model) selectionScreen() string {
 	// The header
 	s := "HackerReader\n\n"
 
 	// Iterate over stories
-	cnt := 0
 	starti := m.cursor - 2
 	if starti < 0 {
 		starti = 0
 	}
-	for i := starti; i < len(m.topstories) && cnt < 5; i++ {
+	for i := starti; i < len(m.topstories) && i < starti+5; i++ {
 		stId := m.topstories[i]
 		st, exists := m.stories[stId]
-		if !exists {
-			continue
-		}
 
 		cursor := " "
-		if m.cursor == cnt {
+		if m.cursor == i {
 			cursor = ">"
 		}
+
 		// Render the row
-		s += fmt.Sprintf("%s %d. %s (%s)\n", cursor, i, st.title, st.domain)
-		s += fmt.Sprintf("     %d points by %s %s | %d comments\n", st.score, st.by, st.timestr, st.descendants)
-		cnt += 1
+		if !exists || st.id < 0 {
+			// still loading/hasn't started loading
+			s += fmt.Sprintf("%s %d. %s (%s)\n", cursor, i, "...", "...")
+			s += fmt.Sprintf("     %d points by %s %s | %d comments\n", 0, "...", "...", 0)
+		} else {
+			s += fmt.Sprintf("%s %d. %s (%s)\n", cursor, i, st.title, st.domain)
+			s += fmt.Sprintf("     %d points by %s %s | %d comments\n", st.score, st.by, st.timestr, st.descendants)
+		}
 	}
 
 	return s
